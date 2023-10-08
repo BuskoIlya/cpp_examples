9b0d5089ad90e9f2f737da7cef6f1b45b5ecf72b
Busko Ilya <trassae95st@mail.ru>
2023-10-08 08:57:14 +0300

    [example_1] Format code

--- example_1/ibusko_example_1.cpp
+++ example_1/ibusko_example_1.cpp
@@ -1,21 +1,25 @@
-﻿#include <iostream>
-#include <ctime>
+﻿#include <ctime>
+#include <iostream>
 
 int main() {
-    int n;
-    std::cin >> n;
-    int* a = new int[n];
+    int arr_size;
+    std::cin >> arr_size;
+
+    int* arr = new int[arr_size];
     srand(time(0));
+    for (int i = 0; i < arr_size; i++) {
+        arr[i] = rand();
+    }
 
-    for (int i = 0; i < n; i++)
-        a[i] = rand();
+    for (int i = 1; i < arr_size - 1; i++) {
+        if (!(((i % 2 == 0) && (arr[i] >= arr[i - 1])) || ((i % 2 != 0) && (arr[i] <= (arr[i - 1]))))) {
+            std::swap(arr[i], arr[i - 1]);
+        }
+    }
 
-    for (int i = 1; i < n - 1; i++)
-        if (!(((i % 2 == 0) && (a[i] >= a[i - 1])) || ((i % 2 != 0) && (a[i] <= (a[i - 1])))))
-            std::swap(a[i], a[i - 1]);
+    for (int i = 0; i < arr_size; i++) {
+        std::cout << arr[i] << " ";
+    }
 
-    for (int i = 0; i < n; i++)
-        std::cout << a[i] << " ";
-
-    delete[] a;
+    delete[] arr;
 }
