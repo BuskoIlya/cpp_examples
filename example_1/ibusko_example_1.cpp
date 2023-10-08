3f3a88909822dcc235971a81d09cf073ebe3853c
Busko Ilya <trassae95st@mail.ru>
2023-10-08 09:03:16 +0300

    [example_1] Create user friendly interface

--- example_1/ibusko_example_1.cpp
+++ example_1/ibusko_example_1.cpp
@@ -2,14 +2,23 @@
 #include <iostream>
 
 int main() {
+    std::setlocale(LC_ALL, "Russian");
+
     int arr_size;
+    std::wcout << L"Введите размер массива: ";
     std::cin >> arr_size;
 
     int* arr = new int[arr_size];
     srand(time(0));
     for (int i = 0; i < arr_size; i++) {
-        arr[i] = rand();
+        arr[i] = rand() % 10;
     }
+
+    std::wcout << L"Был сгенерирован массив:";
+    for (int i = 0; i < arr_size; i++) {
+        std::cout << " " << arr[i];
+    }
+    std::cout << std::endl;
 
     for (int i = 1; i < arr_size - 1; i++) {
         if (!(((i % 2 == 0) && (arr[i] >= arr[i - 1])) || ((i % 2 != 0) && (arr[i] <= (arr[i - 1]))))) {
@@ -17,9 +26,11 @@ int main() {
         }
     }
 
+    std::wcout << L"Массив после обработки:";
     for (int i = 0; i < arr_size; i++) {
-        std::cout << arr[i] << " ";
+        std::cout << " " << arr[i];
     }
+    std::cout << std::endl;
 
     delete[] arr;
 }
