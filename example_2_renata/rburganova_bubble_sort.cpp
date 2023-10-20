3c32e6b809b06859a31edbc3287156fc217533c2
Busko Ilya <trassae95st@mail.ru>
2023-10-20 23:41:21 +0300

    [rburganova] Refactor by Busko Ilya

--- example_2_renata/rburganova_bubble_sort.cpp
+++ example_2_renata/rburganova_bubble_sort.cpp
@@ -1,64 +1,75 @@
-﻿#include <ctime>
+﻿#include <algorithm>
+#include <ctime>
 #include <iostream>
 
-// Camel case <- google
-// Если в каждой функции используется size, то его стоит писать впереди/сзади всегда
-void BubbleSort(const int size, int array[]);
-void RandomArr(const int size, int array[]);
-void StdSort(const int size, const int unsorted_arr[], int std_array[]);
-void Test(const int size, const int bubblesort_array[], const int std_array[]);
+void copy_array(const int* const arr, int* const copy_arr, const int n);
+void init_array(int arr[], const int n);
+bool is_equal_arrays(const int* const arr1, const int* const arr2, const int n);
+void print_array(const wchar_t* const comment, const int* const arr, const int n);
+void sort(int arr[], const int n);
 
 int main() {
     setlocale(LC_ALL, "Russian");
-    const int size = 10; // const
-    int arr[size]; // Создаем массив в main, тк иначе он пропадет из стека после завершения функции (RandomArr)
-    int std_arr[size];
-    RandomArr(size, arr);
-    StdSort(size, arr, std_arr);
-    BubbleSort(size, arr);
-    Test(size, arr, std_arr);
+
+    const int arr_size = 10;
+    int arr[arr_size];
+    int copy_arr[arr_size];
+
+    init_array(arr, arr_size);
+    print_array(L"Сгенерированный массив: ", arr, arr_size);
+
+    copy_array(arr, copy_arr, arr_size);
+    std::sort(copy_arr, copy_arr + arr_size);
+
+    sort(arr, arr_size);
+    bool is_equal = is_equal_arrays(arr, copy_arr, arr_size);
+    if (is_equal) {
+        std::wcout << L"Результаты сошлись" << std::endl;
+    } else {
+        std::wcout << L"Результаты не сошлись" << std::endl;
+    }
+
+    return 0;
 }
 
-void BubbleSort(const int size, int array[]) {
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size - 1; j++) {
-            if (array[j] > array[j + 1]) {
-                std::swap(array[j], array[j + 1]);
-            }
+void copy_array(const int* const arr, int* const copy_arr, const int n) {
+    for (int i = 0; i < n; i++) {
+        copy_arr[i] = arr[i];
+    }
+}
+
+void init_array(int arr[], const int n) {
+    srand(time(0));
+    const int value_limit = 30;
+    for (int i = 0; i < n; i++) {
+        arr[i] = rand() % value_limit;
+    }
+}
+
+bool is_equal_arrays(const int* const arr1, const int* const arr2, const int n) {
+    for (int i = 0; i < n; i++) {
+        if (arr1[i] != arr2[i]) {
+            return false;
         }
     }
+    return true;
 }
 
-void RandomArr(const int size, int array[]) {
-    srand(time(0));
-    for (int i = 0; i < size; i++) {
-        array[i] = rand() % 30;
-    }
-    std::wcout << L"Сгенерированный массив: ";
-    for (int i = 0; i < size; i++) {
-        std::cout << array[i] << " ";
+void print_array(const wchar_t* const comment, const int* const arr, const int n) {
+    const char space = ' ';
+    std::wcout << comment;
+    for (int i = 0; i < n; i++) {
+        std::cout << arr[i] << space;
     }
     std::cout << std::endl;
 }
 
-void StdSort(const int size, const int unsorted_arr[], int std_array[]) {
-    for (int i = 0; i < size; i++) {
-        std_array[i] = unsorted_arr[i];
-    }
-    std::sort(std_array, std_array + size);
-}
-
-void Test(const int size, const int bubblesort_array[], const int std_array[]) {
-    // Нам не нужна bool result
-    for (int i = 0; i < size; i++) {
-        if (bubblesort_array[i] != std_array[i]) {
-            std::wcout << L"Результаты не сошлись";
-            return;
+void sort(int arr[], const int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                std::swap(arr[j], arr[j + 1]);
+            }
         }
     }
-    std::wcout << L"Результаты сошлись" << std::endl;
 }
-
-
-
-
